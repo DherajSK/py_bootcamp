b262c0247bbf7b995a7b0bf5d1db8bd79f8f1b8c
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-03 16:58:54 +0530

    Done

--- Activity_15.py
+++ Activity_15.py
@@ -1,7 +1,17 @@
-x=list(input("Enter strings").split())
-x1=sorted(x)
-print(x)
-print(x1)
-x.sort()
-print(x)
-print(x1)
+import math
+def input_str():
+    return list(input("Enter strings").split())
+def sortUtil(x):
+    x1=sorted(x)
+    display(x)
+    display(x1)
+    x.sort()
+    display(x)
+def display(x):
+    print(x)
+def main():
+    x = input_str()
+    sortUtil(x)
+
+if __name__ == "__main__":
+    main()
