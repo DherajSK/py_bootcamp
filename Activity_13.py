668ca94c2fe05061e3fba29b2ab2d70946a4c5b9
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-03 12:12:00 +0530

    Done

---
+++ Activity_13.py
@@ -0,0 +1,23 @@
+def input_number():
+    return int(input("Enter a number"))
+def isPrime(a):
+    fl=False
+    if a>1:
+        for i in range(2,a):
+            if a%i==0:
+                fl=True
+                break
+    return fl
+def display(a,fl):
+    if not fl:
+        print("{} is Prime".format(a))
+    else:
+        print("{} is non-prime".format(a))
+def main():
+    a = input_number()
+    flag = isPrime(a)
+    display(a,flag)
+
+if __name__ == "__main__":
+    main()
+
