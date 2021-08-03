84387cd385e462c416be867466a6107f26ab3a96
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-03 12:47:59 +0530

    Done

--- Activity_14.py
+++ Activity_14.py
@@ -5,13 +5,11 @@ def input_number():
 def calcVol(l,b,h):
     k=(l**2)+(b**2)+(h**2)
     vol=(b**2)*(h**2)/math.sqrt(k)
-    vol=round(vol,3)
     rad=((3*vol)/(4*math.pi))**(1./3.)
-    rad=round(rad,3)
     return vol,rad
 def display( vol, rad):
-    print("Volume of tromboloid",vol)
-    print("Radius of sphere with above given vol",rad)
+    print("Volume of tromboloid %.3f" %vol)
+    print("Radius of sphere with above given vol %.3f" %rad)
 def main():
     l,b,h = input_number()
     vol,rad = calcVol(l,b,h)
