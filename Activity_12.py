2f91ee3cbf06ceecb203cb7d850b202c9db0b099
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-03 12:12:48 +0530

    Done

--- Activity_12.py
+++ Activity_12.py
@@ -6,8 +6,8 @@ def display( a, res):
     print("{} is the greatest number among {},{},{}".format(res,a[0],a[1],a[2]))
 def main():
     a = input_number()
-    grt = greatest(a)
-    display(a, grt)
+    gret = greatest(a)
+    display(a, gret)
 
 if __name__ == "__main__":
     main()
