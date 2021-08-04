b93cd1d467e6029a49da916be40ed910e42e2d5f
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-04 17:56:38 +0530

    Donee diff ways

--- Activity_04.py
+++ Activity_04.py
@@ -1,3 +1,6 @@
 x1=int(input("Enter number 1"))
 x2=int(input("Enter number 2"))
 print("{}+{}={}".format(x1,x2,x1+x2))
+print(x1,"+",x2,"=",x1+x2)
+print(f'{x1}+{x2}={x1+x2}')
+print("%d+%d=%d"%(x1,x2,x1+x2))
