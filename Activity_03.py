e96b5d8305c195fbc177b09f6c12146594fea68e
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-04 17:25:34 +0530

    Donee

--- Activity_03.py
+++ Activity_03.py
@@ -1,4 +1,4 @@
-x1=input("Enter string 1")
-x2=input("Enter string 2")
-print(x1+x2)
+x1=input("Enter string 1 for concatenation")
+x2=input("Enter string 2 for concatenation")
+print("Concatenated string: ",x1+x2)
 print(5*(x1+x2+ '\n'))
