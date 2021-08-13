ac318eff5f33a6324dbb453089a4e91cc412fdd1
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-13 18:15:40 +0530

    Done

--- Activity_21.py
+++ Activity_21.py
@@ -1,16 +1,27 @@
 import time
-sec=round(time.time())
-print(sec)
-tmin=sec//60
-thrs=tmin//60
-tdays=thrs//24
-tmonths=tdays//30
-print("Total Days:",tdays)
+def gettime():
+    sec=round(time.time())
+    return sec
+def calctime(sec):
+    tmin=sec//60
+    thrs=tmin//60
+    tdays=thrs//24
+    
+    years=tdays//365
+    remdays=tdays%365
+    months=remdays//30
+    days=tdays%30
+    return years, months, days, tdays
 
-years=tdays//365;
-remdays=tdays%365
-months=remdays//30
-days=tdays%30
-print(years, "Years ",months,"Months ",days,"Days")
-#challenge
-print("Today's date:",1+days,"/",1+months,"/",1970+years)
+def display(years,months,days,tdays):
+    print("Total Days:",tdays)
+    print(years, "Years ",months,"Months ",days,"Days")
+    #challenge
+    print("Today's date:",1+days,"/",1+months,"/",1970+years)
+
+def main():
+    y,m,d,td=calctime(gettime())
+    display(y,m,d,td)
+
+if __name__ == "__main__":
+    main()
