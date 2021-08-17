651e5ffd5f331897fbc5002f27dba511e9e75888
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-17 19:40:56 +0530

    Done 2

--- Activity_09.py
+++ Activity_09.py
@@ -1,7 +1,19 @@
 import math
-l,b,h=map(float,input("Enter l,b,h").split())
-k=(l**2)+(b**2)+(h**2)
-vol=(b**2)*(h**2)/math.sqrt(k)
-print("Volume of tromboloid %.3f"% vol)
-rad=((3*vol)/(4*math.pi))**(1/3)
-print("Radius of sphere with above given vol %.3f"% rad)
+def getparam():
+    l,b,h=map(float,input("Enter l,b,h").split())
+    return l, b, h
+def compute(l,b,h):
+    k=(l**2)+(b**2)+(h**2)
+    vol=(b**2)*(h**2)/math.sqrt(k)
+    rad=((3*vol)/(4*math.pi))**(1/3)
+    return  vol, rad
+def display(vol,rad):
+    print("Volume of tromboloid %.3f"% vol)
+    print("Radius of sphere with above given vol %.3f"% rad)
+
+def main():
+    l,b,h=getparam()
+    vol,rad=compute(l,b,h)
+    display(vol,rad)
+
+main()
