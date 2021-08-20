71661809558c4f8a0ec04ae172c12fbd7e2bf9d7
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-08-20 16:47:17 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,7 +1,8 @@
 def input_number():
     return list(map(int,input("Enter three numbers").split()))
 def greatest(a):
-    return max(a[0],max(a[1],a[2]))
+    return a[0] if(a[0]>a[1])and(a[0]>a[2]) else a[1] if(a[1]>a[2])and(a[1]>a[0]) else a[2]	
+    #return max(a[0],max(a[1],a[2]))
 def display( a, res):
     print("{} is the greatest number among {},{},{}".format(res,a[0],a[1],a[2]))
 def main():
